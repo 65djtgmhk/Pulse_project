2e6b53ee5c322ed658520cc1a67ee7b54be1cb25
Danil <tresk33@gmail.com>
2022-12-04 11:24:46 +0300

    added validation forms

--- src/js/script.js
+++ src/js/script.js
@@ -47,4 +47,35 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow');
         })
     });
+    //Валидация форм
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlength: jQuery.validator.format("Введите  {0} символа")
+                  },
+                phone: "Пожалуйста, введите свой телефон",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Ваш адрес должен выглядеть так:name@domain.com"
+                }
+              }
+            
+        });
+    };
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
 });
